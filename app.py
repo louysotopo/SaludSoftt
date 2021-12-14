04ab94fc7538aa823aca9c02208ab9f4c3423229
louysotopo <louysotopo123@gmail.com>
2021-12-14 00:44:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, app,jsonify,request;
+from flask_cors import CORS;
 from flask_mail import Mail, Message 
 
 app = Flask(__name__)
+CORS(app)
+
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
